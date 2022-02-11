e81615d5541ad546a05e57d48783a474f1097b6b
olhavalchak <o.valchak@do-it.co>
2022-02-11 12:52:52 +0200

    Add custom hook.

--- src/components/TodoList/index.jsx
+++ src/components/TodoList/index.jsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import React from 'react';
+import { useTasks } from '../../hooks';
 import edit from '../../theme/assets/edit.png';
 import destroy from '../../theme/assets/destroy.png';
 import done from '../../theme/assets/done.png';
@@ -12,53 +12,20 @@ import {
   StyledFilterContainer,
   StyledButton,
 } from '../../pages/TaskList/elements';
-import { tasksActions } from '../../lib/redux/actions';
-import { selectTasks } from '../../lib/redux/selectors';
 
 export const TodoList = () => {
-  const tasks = useSelector(selectTasks);
-  const dispatch = useDispatch();
-  const [editingTaskId, setEditingTaskId] = useState(null);
-  const [changingTask, setChangingTask] = useState('');
-  const FILTER_STATUSES = { ALL: 'All', ACTIVE: 'Active', FINISHED: 'Finished' };
-  const [filter, setFilter] = useState(FILTER_STATUSES.ALL);
-
-  useEffect(() => {
-    dispatch(tasksActions.getTasksAsync());
-  }, []);
-
-  const toggleTask = (task) => {
-    dispatch(tasksActions.updateTaskAsync(task.id, { ...task, isFinished: !task.isFinished }));
-  };
-
-  const onTaskChange = (event) => {
-    setChangingTask(event.target.value);
-  };
-
-  const updateTask = (task) => {
-    dispatch(tasksActions.updateTaskAsync(task.id, { ...task, task: changingTask }));
-    setEditingTaskId(null);
-  };
-
-  const onEdit = (taskText, taskId) => {
-    setEditingTaskId(taskId);
-    setChangingTask(taskText);
-  };
-
-  const deleteTask = (taskId) => {
-    dispatch(tasksActions.deleteTaskAsync(taskId));
-  };
-
-  const filterTasks = () => tasks.filter((task) => {
-    switch (filter) {
-      case FILTER_STATUSES.ACTIVE:
-        return !task.isFinished;
-      case FILTER_STATUSES.FINISHED:
-        return task.isFinished;
-      default:
-        return task;
-    }
-  });
+  const {
+    editingTaskId,
+    changingTask,
+    FILTER_STATUSES,
+    setFilter,
+    toggleTask,
+    onTaskChange,
+    updateTask,
+    onEdit,
+    deleteTask,
+    filterTasks,
+  } = useTasks();
 
   return (
     <>
